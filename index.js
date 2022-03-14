a01c08bf41e261b2b5aa00a3f8c4f33f60fb203b
Paolo Cargnin <paolo.cargnin@h-is.com>
2022-03-14 12:35:21 +0100

    your turn nbow!

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// Dependencies
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
+
+// create the app
 const app =  express()
-const mongoDbURI = 'mongodb+srv://test:test@cluster0.1vosb.mongodb.net/blogdatabase?retryWrites=true&w=majority'
 
+// enable POST request
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// Install ejs - to show JS variables into the HTML
+app.set('view engine','ejs')
+
+// Connect to mongoDb
+const mongoDbURI = 'mongodb+srv://test:test@cluster0.1vosb.mongodb.net/blogdatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoDbURI)
 
+
+// Connect the Models
 require('./models/Article')
+
+
+// Connect the routes
 app.use(require('./routes/frontend'))
-app.set('view engine','ejs')
 
 
 const frontendRoutes = require('./routes/frontend')
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,10 +1,16 @@
+// dependencies 
 const express = require('express')
-const res = require('express/lib/response')
 const mongoose = require('mongoose')
+
+// Get the models
 const Article = mongoose.model('articles')
 
+
+// create the routes
 const router = express.Router()
 
+// create the request
+//list of the articles
 router.get('/', async (req,res) => {
   // render the list of articles
   const articles = await Article.find().lean()
@@ -14,10 +20,21 @@ router.get('/', async (req,res) => {
   })
 })
 
+router.post('/articles/create', async (req,res) => {
+  // create  a new document on the database
+  await Article.create(req.body)
+  // console.log(req.body)
+  res.render('new-article', {})
+})
+
+router.get('/articles/create', (req,res) => {
+  res.render('new-article', {})
+})
 
 
 router.get('/articles/:id', () => {
   
 })
 
+// export the requests
 module.exports = router;
